456ac97c916edea7bdf6052457e2551679f55b46
Mario Jacobo <mario_jacobo@uri.edu>
2021-03-25 22:53:44 -0400

    modified helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,1 +1,8 @@
-print("Hello World, my name is Mario Jacobo and I love Python!")
+userName = input("Hello! Please enter your facebook username! ")
+
+userPassword = input("Hello! Please enter your facebook password! ")
+
+if(userName.lower() == "mario" and userPassword == "Corona_2020"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong credentials, try again!")
